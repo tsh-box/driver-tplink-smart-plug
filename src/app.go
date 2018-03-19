cd51fa1c90e59bf79656eb303e9028f70393f02b
lae <laelaeaung007@gmail.com>
2018-03-19 10:23:57 +0900

    change tsh2

--- src/app.go
+++ src/app.go
@@ -10,7 +10,7 @@ import (
 	"./plugs"
 
 	"github.com/gorilla/mux"
-	databox "github.com/toshbrown/lib-go-databox"
+	databox "github.com/tsh2/lib-go-databox"
 )
 
 var dataStoreHref = os.Getenv("DATABOX_STORE_ENDPOINT")
